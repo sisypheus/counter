6aaef3e2ae5a3a2159d24a05e552c68bec59ce1e
tpoette <theo.poette@epitech.eu>
2020-07-28 22:03:15 +0200

    [+] counter done with reset button and appropriate colors, needs a reorganization of function and remove clutter

--- js/index.js
+++ js/index.js
@@ -1,6 +1,35 @@
 let clicks = document.getElementById('clicks');
+let increase = document.getElementById('increase');
+let decrease = document.getElementById('decrease');
+let restore = document.getElementById('reset');
 
 let nbclicks = 0;
 
-//clicks.style.color = 'red';
 clicks.innerHTML = nbclicks;
+
+restore.addEventListener('click', () => {
+    nbclicks = 0;
+    clicks.innerHTML = nbclicks;
+    color(nbclicks, clicks);
+});
+
+decrease.addEventListener('click', () => {
+    nbclicks--;
+    clicks.innerHTML = nbclicks;
+    color(nbclicks, clicks);
+});
+
+increase.addEventListener('click', () => {
+    nbclicks++;
+    clicks.innerHTML = nbclicks;
+    color(nbclicks, clicks);
+});
+
+function color(nbclicks, element) {
+    if (nbclicks < 0)
+        element.style.color = 'red';
+    else if (nbclicks > 0)
+        element.style.color = 'green';
+    else
+        element.style.color = 'black';
+}
